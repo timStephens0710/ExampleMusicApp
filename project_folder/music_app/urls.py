c6256a23051e5b8ea6c4468fa66297fb0a39cd79
timStephens0710 <timothy.stephens0710@gmail.com>
2025-10-16 12:22:09 +0100

    creating front-end aestheic

--- project_folder/music_app/urls.py
+++ project_folder/music_app/urls.py
@@ -19,21 +19,21 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .import views
+from .views import main_views, app_views
 
 urlpatterns = [
     path('admin_tools_stats/', include('admin_tools_stats.urls'))
     , path('admin/', admin.site.urls)
-    , path('', views.home, name = "music_app_home")
-    , path('registration/', views.user_registration, name = "user_registration")
-    , path('login/', views.user_login, name = 'user_login')
-    , path('user_authentication/<int:user_id>/', views.user_authentication, name = "user_authentication")
-    , path('user_authentication_success/<int:user_id>/<uuid:token>/', views.user_authentication_success, name = "user_authentication_success")
-    , path('the_feed/', views.the_feed, name = "the_feed")
-    , path('logout/', views.user_logout, name = "user_logout")
-    , path('user_forgotten_password/', views.user_forgotten_password, name = 'user_forgotten_password')
-    , path('check_your_email_password/<int:user_id>/', views.check_your_email_password, name = 'check_your_email_password')
-    , path('user_reset_password/<int:user_id>/<uuid:token>/', views.user_reset_password, name = 'user_reset_password')
-    , path('success_reset_password/', views.user_success_reset_password, name = 'user_success_reset_password')
+    , path('', main_views.home, name = "music_app_home")
+    , path('registration/', main_views.user_registration, name = "user_registration")
+    , path('login/', main_views.user_login, name = 'user_login')
+    , path('user_authentication/<int:user_id>/', main_views.user_authentication, name = "user_authentication")
+    , path('user_authentication_success/<int:user_id>/<uuid:token>/', main_views.user_authentication_success, name = "user_authentication_success")
+    , path('the_feed/', app_views.the_feed, name = "the_feed")
+    , path('logout/', main_views.user_logout, name = "user_logout")
+    , path('user_forgotten_password/', main_views.user_forgotten_password, name = 'user_forgotten_password')
+    , path('check_your_email_password/<int:user_id>/', main_views.check_your_email_password, name = 'check_your_email_password')
+    , path('user_reset_password/<int:user_id>/<uuid:token>/', main_views.user_reset_password, name = 'user_reset_password')
+    , path('success_reset_password/', main_views.user_success_reset_password, name = 'user_success_reset_password')
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
