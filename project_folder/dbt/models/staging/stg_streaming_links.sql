9f94faf5973c3eeee33c70e93ffa03f5105c6b69
Tim Stephens <timothy.stephens0710@gmail.com>
2026-03-02 15:12:09 +0000

    create staging files + dtb_project.yml

---
+++ project_folder/dbt/models/staging/stg_streaming_links.sql
@@ -0,0 +1,11 @@
+/*
+    This is the staging model for the StreamingLink model.
+
+    It will be referenced in the following mart model:
+        - mart_xxxx
+
+*/
+
+SELECT id,
+       streaming_platform
+FROM {{ source('raw', 'streaming_links')}}
